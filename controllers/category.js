d8a464c012f22b62d19c126695841247d44936a8
whytdan <aktan.r.a@gmail.com>
2021-08-02 20:10:35 +0600

    Add default mongo query code to retrieve list of home and category items from db

--- controllers/category.js
+++ controllers/category.js
@@ -1,3 +1,37 @@
-exports.categories_list = (req, res) => {
-  res.send('NOT IMPLEMENTED: Categories list');
+const client = require('../utils/mongoSetup');
+
+require('dotenv').config();
+
+exports.categories_list = async (req, res) => {
+  const query = req.query;
+  const page = (+query['_page'] ?? 1) - 1;
+  const limit = +query['_limit'];
+
+  const resultFilterSchema = {};
+  const filterParams = [
+    'flexibleCancellation',
+    'instanceBooking',
+    'housingType',
+    'price_gte',
+    'price_lte',
+  ];
+
+  Object.entries(query).map(([key, value]) => {
+    if (filterParams.includes(key)) {
+      resultFilterSchema[key] = JSON.parse(value);
+    }
+  });
+
+  await client.connect();
+  const db = client.db(process.env.DB_NAME);
+  db.collection('homes')
+    .find({
+      ...resultFilterSchema,
+    })
+    .skip(page * 5)
+    .limit(limit)
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
 };
--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,14 @@
-exports.homes_list = (req, res) => {
-  res.send('NOT IMPLEMENTED: Homes list');
+const client = require('../utils/mongoSetup');
+
+require('dotenv').config();
+
+exports.homes_list = async (req, res) => {
+  await client.connect();
+  const db = client.db('airbnb-db');
+  db.collection('categories')
+    .find({})
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
 };
